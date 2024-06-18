801b80a7e7b813351cf95525f9ba9a06b4d7947d
flameshikari <mail@hexed.pw>
2024-06-18 13:31:08 +0500

    update the script

--- utils/check_missing_lines.py
+++ utils/check_missing_lines.py
@@ -8,18 +8,11 @@ def workdir(path):
 
 en_json_path = workdir('../src/shared/i18n/locales/en_US/translation.json')
 ru_json_path = workdir('../shared/i18n/locales/ru_RU/translation.json')
-merged_json_path = workdir('./merged.json')
+out_json_path = workdir('./translation.json')
 
 translated_lines = {}
 untranslated_lines = {}
-
-exceptions = [
-    'Google Analytics',
-    'HTML',
-    'Markdown',
-    'PDF',
-    'URL'
-]
+exception_lines = {}
 
 with open(en_json_path) as target:
     en_json = json.load(target)
@@ -28,17 +21,30 @@ with open(ru_json_path) as target:
     ru_json = json.load(target)
 
 for key, value in en_json.items():
-    if key in exceptions:
-        translated_lines[key] = en_json[key]
     if key in ru_json.keys():
         translated_lines[key] = ru_json[key]
     else:
         untranslated_lines[key] = en_json[key]
 
-merged_json = {**translated_lines, **untranslated_lines}
 
-print(json.dumps(untranslated_lines, indent=2, ensure_ascii=False))
+for key, value in ru_json.items():
+    if key == value:
+        exception_lines[key] = value
+    
 
-with open(merged_json_path, 'w') as target:
-    obj = json.dumps(merged_json, indent=2, ensure_ascii=False)
-    target.write(obj)
+out_json = {**translated_lines, **untranslated_lines}
+
+if (exception_lines):
+    print('Потенциально непереведённые строки или исключения:')
+    print(json.dumps(exception_lines, indent=2, ensure_ascii=False))
+    print()
+
+if (untranslated_lines):
+    print('Непереведённые строки:')
+    print(json.dumps(untranslated_lines, indent=2, ensure_ascii=False))
+    print()
+
+with open(out_json_path, 'w') as target:
+    obj = json.dumps(out_json, indent=2, ensure_ascii=False)
+    target.write(obj + '\n')
+    print('Переведенные и непереведенные строки смержены в translation.json')
